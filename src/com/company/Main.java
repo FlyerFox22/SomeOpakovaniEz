afedd260db1fa939567cb529986d0c70f15d637c
tomas.zamecky <tomasus158@gmail.com>
2021-09-17 09:23:13 +0200

    Pokus cislo XX

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String veta = sc.nextLine();
+        int a = 0;
+        int b = 0;
         String[] pole = veta.split("");
         int soucet = 0;
         for (String i : pole) {
             if (i.equals ("ahoj")) soucet++;
         }
         System.out.println(soucet);
+        if (a + b < 5) {
+            System.out.println("Je to víc jak 5!");
+        }
+        else {
+            System.out.println("Není to víc jak 5!");
+        }
 
 
 
